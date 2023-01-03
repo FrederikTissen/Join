60267ef35c942724686032a5bbabe619e4849b7c
Kubilay Özmen <kubilay.oe@hotmail.de>
2023-01-03 17:24:54 +0100

    Tasks as Array
    And some little change on the path from script.js to js/script.js

--- js/scriptAddTask.js
+++ js/scriptAddTask.js
@@ -1,8 +1,40 @@
+let allTasks = [];
+
+
 var popupWindow = null;
-function centeredPopup(url,winName,w,h,scroll){
-LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
-TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
-settings =
-'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
-popupWindow = window.open(url,winName,settings)
+
+
+function centeredPopup(url, winName, w, h, scroll) {
+    LeftPosition = (screen.width) ? (screen.width - w) / 2 : 0;
+    TopPosition = (screen.height) ? (screen.height - h) / 2 : 0;
+    settings =
+        'height=' + h + ',width=' + w + ',top=' + TopPosition + ',left=' + LeftPosition + ',scrollbars=' + scroll + ',resizable'
+    popupWindow = window.open(url, winName, settings)
+}
+
+
+function addTask() {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let date = document.getElementById('date').value;
+
+    
+
+    let task = {
+        'title': title,
+        'description': description,
+        'date': date,
+    };
+
+    allTasks.push(task);
+
+    let allTasksAsString = JSON.stringify(allTasks);
+    localStorage.setItem('allTasks', allTasksAsString);
+
+}
+
+
+function loadAllTasks(){
+   let allTasksAsString = localStorage.getItem('allTasks');
+   allTasks = JSON.parse(allTasksAsString);
 }
