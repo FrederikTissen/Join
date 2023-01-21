87f3b864e08344303f1ab90426dcb1402b895695
FrederikTissen <frederiktissen@web.de>
2023-01-21 20:10:23 +0100

    update AddTask until Prio

--- js/scriptAddTask.js
+++ js/scriptAddTask.js
@@ -1,13 +1,29 @@
 let allTasks = [];
 let selectionPrio;
 let allCategories = [];
+let allContacts = [];
+
+
+function onload() {
+    loadAllTasks();
+    loadAllContacts();
+    renderCategoryBox();
+    renderContactBox();
+    clock();
+}
+
+function clock(){
+var date = new Date();
+var currentDate = date.toISOString().slice(0,10);
+
+document.getElementById('date').value = currentDate;
+}
 
 function addTask() {
     let title = document.getElementById('title');
     let description = document.getElementById('description');
     let date = document.getElementById('date');
     let selectionCategory = document.getElementById('categorySelect');
-
 
     let task = {
         'title': title.value,
@@ -16,36 +32,12 @@ function addTask() {
         'categorySelect': selectionCategory.value,
         'prioSelect': selectionPrio,
     };
-
-    console.log(task);
-
-
     allTasks.push(task);
 
     saveTask();
 }
 
-function renderTasks() {
 
-    loadAllTasks();
-    document.getElementById('todo-box').innerHTML = '';
-
-    for (let i = 0; i < allTasks.length; i++) {
-        currentTask = allTasks[i];
-        let title = allTasks[i]['title'];
-        let description = allTasks[i]['description'];
-
-        let newTask = document.getElementById('todo-box');
-
-
-        newTask.innerHTML += /*html*/ `
-            <div onclick="openShowTask(${i})" class="current-task">
-                <p>${title}</p>
-                <p>${description}</p>
-            </div>
-        `
-    }
-}
 
 function renderCategories() {
     loadAllCategories();
@@ -53,29 +45,35 @@ function renderCategories() {
     document.getElementById('category').innerHTML = /*html*/ `
     <span>Category</span><br><br>
     <div  id="category-box" class="category-box">
-        <p onclick="closeSelection()" class="select-category">Select task category</p>
+        <p onclick="renderCategoryBox()" class="select-category">Select task category</p>
         <div onclick="openInput()" class="selection-category">
             <div>New category</div>
         </div>
     </div>
     `;
 
-if(allCategories.length < 0) {
-    for (let i = 0; i < allCategories.length;) {
+    for (let i = 0; i < allCategories.length; i++) {
         currentCategory = allCategories[i];
         let category = allCategories[i]['categoryName'];
         /*let color = allCategories[i]['color'];*/
-
         let newCategory = document.getElementById('category-box');
-
 
         newCategory.innerHTML += /*html*/ `
             <div class="selection-category">
-                <div>${category}</div>
+                <div id="category${i}" onclick="acceptCategory(${i})">${category}</div>
             </div>
         `
     }
 }
+
+function acceptCategory(i) {
+    selectedCategory = document.getElementById(`category${i}`);
+    document.getElementById('category').innerHTML = /*html*/ `
+    <span>Category</span><br><br>
+    <div onclick="renderCategories()" id="category-box" class="category-box">
+                        <p class="select-category">${selectedCategory.innerHTML}</p>
+                    </div>
+    `;
 }
 
 
@@ -85,7 +83,7 @@ function openInput() {
     <span>Category</span><br><br>
     <input id="input-category"  placeholder="New category name" class="addTotaskInputField" type="text">
     <div id="input-nav-box" class="input-nav-box">
-                            <img onclick="closeInputCategory()" class="x-black" src="/assets/img/x-black.png">
+                            <img onclick="renderCategoryBox()" class="x-black" src="/assets/img/x-black.png">
                             <img class="line" src="/assets/img/line.png">
                             <img onclick="pushNewCategory()" class="hook" src="/assets/img/hook.png">
                         </div>
@@ -99,7 +97,6 @@ function pushNewCategory() {
         'categoryName': categoryName.value,
     };
 
-    
 
 
     allCategories.push(newCategory);
@@ -111,10 +108,10 @@ function pushNewCategory() {
 
 }
 
-function closeInputCategory() {
+function renderCategoryBox(i) {
     document.getElementById('category').innerHTML = '';
     document.getElementById('category').innerHTML = /*html*/ `
-    <span>Category</span>
+    <span>Category</span><br><br>
     <div onclick="renderCategories()" id="category-box" class="category-box">
                         <p class="select-category">Select task category</p>
                     </div>
@@ -133,46 +130,91 @@ function choosePrio(prio) {
     }
 }
 
+function renderContacts() {
+    loadAllContacts();
+
+    document.getElementById('assignedTo').innerHTML = /*html*/ `
+        <span>Assigned to</span><br><br>
+    <div  id="contact-box" class="category-box">
+        <p onclick="renderContactBox()" class="select-category">Select contacts to assign</p>
+        <div onclick="openInputContact()" class="selection-category">
+            <div>Invite new contact</div>
+        </div>
+    </div>
+    `;
+
+    for (let i = 0; i < allContacts.length; i++) {
+        currentContact = allContacts[i];
+        let contact = allContacts[i]['contactName'];
+        /*let color = allCategories[i]['color'];*/
+        let newContact = document.getElementById('contact-box');
+
+        newContact.innerHTML += /*html*/ `
+            <div class="selection-contacts">
+                <div id="contact${i}" onclick="acceptContact(${i})">${contact}</div>
+                <input type="checkbox" id="assignedTo${i}" name="assignedTo${i}" value="assignedTo${i}">
+            </div>
+        `
+    }
+}
+
+function acceptContact(i) {
+    selectedContact = document.getElementById(`contact${i}`);
+    document.getElementById('assignedTo').innerHTML = /*html*/ `
+    <span>Assigned to</span><br><br>
+    <div onclick="renderContacts()" id="contact-box" class="category-box">
+                        <p class="select-category">${selectedContact.innerHTML}</p>
+                    </div>
+    `;
+}
+
+
+function openInputContact() {
+    document.getElementById('assignedTo').innerHTML = '';
+    document.getElementById('assignedTo').innerHTML = /*html*/ `
+    <span>Assigned to</span>
+    <input id="input-contact"  placeholder="New contact" class="addTotaskInputField" type="text">
+    <div id="input-nav-box-contact" class="input-nav-box">
+                            <img onclick="renderContactBox()" class="x-black" src="/assets/img/x-black.png">
+                            <img class="line" src="/assets/img/line.png">
+                            <img onclick="pushNewContact()" class="hook" src="/assets/img/hook.png">
+                        </div>
+    `;
+}
+
+function pushNewContact() {
+    let contactName = document.getElementById('input-contact');
+
+    let newContact = {
+        'contactName': contactName.value,
+    };
+
+    allContacts.push(newContact);
+
+    saveContact();
+    loadAllContacts();
+
+    document.getElementById('input-contact').value = '';
+
+}
+
+function renderContactBox(i) {
+    document.getElementById('assignedTo').innerHTML = '';
+    document.getElementById('assignedTo').innerHTML = /*html*/ `
+    <span>Assigned to</span><br><br>
+    <div onclick="renderContacts()" id="contact-box" class="category-box">
+                        <p class="select-category">Select contact to assign</p>
+                    </div>
+    `;
+}
+
 function closeShowTask() {
     document.getElementById('show-Task-Background').classList = 'show-Task-Background d-none';
 }
 
-function closeSelection() {
-    document.getElementById('category').innerHTML = '';
-    document.getElementById('category').innerHTML = /*html*/ `
-    <span>Category</span><br><br>
-    <div onclick="renderCategories()" id="category-box" class="category-box">
-        <p class="select-category">Select task category</p>
-    </div>
-    `;
-}
 
-function openShowTask(i) {
-    let title = allTasks[`${i}`]['title'];
-    let description = allTasks[`${i}`]['description'];
-    let date = allTasks[`${i}`]['date'];
 
-    document.getElementById('show-Task-Background').classList = 'show-Task-Background';
 
-    document.getElementById('showTask').innerHTML = /*html*/ `
-                <p>Category</p>
-                <h3>${title}</h3>
-                <p>${description}</p>
-                <p>Due date: ${date}</p>
-                <p>Priority: </p>
-                <p>Assigned To: </p>
-
-                <button onclick="deleteTask(${i})">delete</button>
-                <button onclick="closeShowTask()">close</button>
-    `
-}
-
-function deleteTask(i) {
-    allTasks.splice(i, 1);
-    save();
-    closeShowTask();
-    renderTasks();
-}
 
 function saveTask() {
     let allTasksAsString = JSON.stringify(allTasks);
@@ -184,6 +226,8 @@ function saveCategory() {
     localStorage.setItem('allCategories', allCategoriesAsString);
 }
 
+
+
 function loadAllCategories() {
     let allCategoriesAsString = localStorage.getItem('allCategories');
     if (allCategoriesAsString) {
@@ -191,6 +235,17 @@ function loadAllCategories() {
     }
 }
 
+function saveContact() {
+    let allContactsAsString = JSON.stringify(allContacts);
+    localStorage.setItem('allContacts', allContactsAsString);
+}
+
+function loadAllContacts() {
+    let allContactsAsString = localStorage.getItem('allContacts');
+    if (allContactsAsString) {
+        allContacts = JSON.parse(allContactsAsString);
+    }
+}
 
 function loadAllTasks() {
     let allTasksAsString = localStorage.getItem('allTasks');
@@ -198,6 +253,26 @@ function loadAllTasks() {
         allTasks = JSON.parse(allTasksAsString);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function openSubTaskMask() {
---
+++ js/scriptboard.js
@@ -0,0 +1,48 @@
+function renderTasks() {
+
+    loadAllTasks();
+    document.getElementById('todo-box').innerHTML = '';
+
+    for (let i = 0; i < allTasks.length; i++) {
+        currentTask = allTasks[i];
+        let title = allTasks[i]['title'];
+        let description = allTasks[i]['description'];
+
+        let newTask = document.getElementById('todo-box');
+
+
+        newTask.innerHTML += /*html*/ `
+            <div onclick="openShowTask(${i})" class="current-task">
+                <p>${title}</p>
+                <p>${description}</p>
+            </div>
+        `
+    }
+}
+
+function openShowTask(i) {
+    let title = allTasks[`${i}`]['title'];
+    let description = allTasks[`${i}`]['description'];
+    let date = allTasks[`${i}`]['date'];
+
+    document.getElementById('show-Task-Background').classList = 'show-Task-Background';
+
+    document.getElementById('showTask').innerHTML = /*html*/ `
+                <p>Category</p>
+                <h3>${title}</h3>
+                <p>${description}</p>
+                <p>Due date: ${date}</p>
+                <p>Priority: </p>
+                <p>Assigned To: </p>
+
+                <button onclick="deleteTask(${i})">delete</button>
+                <button onclick="closeShowTask()">close</button>
+    `
+}
+
+function deleteTask(i) {
+    allTasks.splice(i, 1);
+    save();
+    closeShowTask();
+    renderTasks();
+}
