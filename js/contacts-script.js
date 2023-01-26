9c9efce298f672cb23040c58f62f1fa742e9397b
Fel1xR0eder <xfeli537@gmail.com>
2023-01-26 13:01:23 +0100

    select color for new contact

--- js/contacts-script.js
+++ js/contacts-script.js
@@ -2,45 +2,62 @@ let contacts = [{
     name: "Mayer",
     firstName: "Anton",
     mail: "antom@gmail.com",
-    phone: "015137294741"
+    phone: "015137294741",
+    color: "#0038FF"
 }, {
     name: "Schulz",
     firstName: "Anja",
     mail: "schulz@hotmail.com",
-    phone: "015137294742"
+    phone: "015137294742",
+    color: "#E201BE"
 }, {
     name: "Eisenberg",
     firstName: "David",
     mail: "davidberg@gmail.com",
-    phone: "015137294743"
+    phone: "015137294743",
+    color: "#FF8A01"
 }, {
     name: "Ziegler",
     firstName: "Benedikt",
     mail: "benedikt@gmail.com",
-    phone: "015137294744"
+    phone: "015137294744",
+    color: "#29D300"
 }, {
     name: "Fischer",
     firstName: "Eva",
     mail: "eva@gmail.com",
-    phone: "015137294745"
+    phone: "015137294745",
+    color: "#FF0100"
 }, {
     name: "Mauer",
     firstName: "Emanuel",
     mail: "emmanuelMa@gmail.com",
-    phone: "015137294746"
+    phone: "015137294746",
+    color: "#8BA4FF"
 }, {
     name: "Bauer",
     firstName: "Marcel",
     mail: "bauer@gmail.com",
-    phone: "015137294747"
+    phone: "015137294747",
+    color: "#0038FF"
 }, {
     name: "Wolf",
     firstName: "Tanja",
     mail: "wolf@gmail.com",
-    phone: "015137294748"
+    phone: "015137294748",
+    color: "#E201BE"
 }];
 let createdContact = true;
 let contactColor = true;
+
+/*
+    blue = #0038FF
+    pink = #E201BE
+    orange = #FF8A01
+    green = #29D300
+    red = #FF0100
+    turquoise = #8BA4FF
+*/
 
 
 async function init() {
@@ -88,11 +105,11 @@ function renderAllContacts() {
     contactSection.innerHTML = '';
 
     for (let i = 0; i < contacts.length; i++) {
-        let randomColor = setRandomColor();
+        let color = contacts[i]['color'];
         let contact = contacts[i];
         let firstChar = contact['firstName'].charAt(0);
         let secondChar = contact['name'].charAt(0);
-        contactSection.innerHTML += generateAllContacts(contact, firstChar, secondChar, i, randomColor);
+        contactSection.innerHTML += generateAllContacts(contact, firstChar, secondChar, i, color);
     }
 }
 
@@ -101,11 +118,11 @@ function renderAllContacts() {
  *
  * @returns
  */
-function generateAllContacts(contact, firstChar, secondChar, i, randomColor) {
+function generateAllContacts(contact, firstChar, secondChar, i, color) {
     return `
     <div id="char-section${i}" class="first-char">${firstChar}</div>
-    <div onclick="showContact(${i}, '${randomColor}')" id="contact-card${i}" class="contact-card">
-        <div id="contact-img${i}" class="contact-img" style='background-color: ${randomColor};'>${firstChar} ${secondChar}</div>
+    <div onclick="showContact(${i}, '${color}')" id="contact-card${i}" class="contact-card">
+        <div id="contact-img${i}" class="contact-img" style='background-color: ${color};'>${firstChar} ${secondChar}</div>
         <div id="contactInfo${i}" class="contact-info">
             <span>${contact['firstName']}  ${contact['name']}</span>
             <p>${contact['mail']}</p>
@@ -118,11 +135,11 @@ function generateAllContacts(contact, firstChar, secondChar, i, randomColor) {
  * 
  * 
  */
-function showContact(i, randomColor) {
+function showContact(i, color) {
     let firstChar = contacts[i]['firstName'].charAt(0);
     let secondChar = contacts[i]['name'].charAt(0);
     let contactfield = document.getElementById('show-contact');
-    contactfield.innerHTML = generateContactfield(i, firstChar, secondChar, randomColor);
+    contactfield.innerHTML = generateContactfield(i, firstChar, secondChar, color);
 }
 
 /**
@@ -131,10 +148,10 @@ function showContact(i, randomColor) {
  * 
  * @returns 
  */
-function generateContactfield(i, firstChar, secondChar, randomColor) {
+function generateContactfield(i, firstChar, secondChar, color) {
     return `
     <div class="show-contact-headline">
-        <div id="contact-img${i}" class="contact-img-big" style="background-color:${randomColor}">${firstChar} ${secondChar}</div>
+        <div id="contact-img${i}" class="contact-img-big" style="background-color:${color}">${firstChar} ${secondChar}</div>
         <div class="show-contact-headline-right"> 
             <div>${contacts[i]['firstName']} ${contacts[i]['name']}</div>
             <div id="add-task" class="blue-font"> + Add Task </div>
@@ -142,7 +159,7 @@ function generateContactfield(i, firstChar, secondChar, randomColor) {
     </div>
     <div class="show-contact-middle">
         <span>Contact Information</span> 
-        <div class="edit-contact" onclick="editContact(${i}, '${randomColor}')">
+        <div class="edit-contact" onclick="editContact(${i}, '${color}')">
             <img style="width: 30px; height: 30px; object-fit: contain;" src="./assets/img/pen.png"><p> Edit Contact</p>
         </div>
     </div>
@@ -172,17 +189,17 @@ function addNewContact() {
  * Pop-up window to edit a contact
  * 
  */
-async function editContact(i, randomColor) {
+async function editContact(i, color) {
     if (createdContact) {
         document.getElementById('show-contact').innerHTML = `
         <div w3-include-html="edit-contact.html"></div>`;
         await includeHTMLaddContact();
     }
-    editContactValues(i, randomColor);
+    editContactValues(i, color);
     createdContact = false;
 }
 
-function editContactValues(i, randomColor) {
+function editContactValues(i, color) {
     let editLastname = document.getElementById(`edit-input-lastname`);
     let editFirstname = document.getElementById(`edit-input-firstname`);
     let editMail = document.getElementById(`edit-input-mail`);
@@ -197,7 +214,7 @@ function editContactValues(i, randomColor) {
     editMail.value = contacts[i]['mail'];
     editPhone.value = contacts[i]['phone'];
     editImage.innerHTML += `${firstChar} ${secondChar}`;
-    editImage.style = `background-color:${randomColor};`;
+    editImage.style = `background-color:${color};`;
 }
 
 async function saveEditContact(i) {
